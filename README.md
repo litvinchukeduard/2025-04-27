ab8e237c8d9c81be0a75c2feb97499a5aec31416
Eduard Litvinchuk <litvinchuckeduard@gmail.com>
2025-04-27 20:11:47 +0300

    Base application

---
+++ README.md
@@ -0,0 +1,5 @@
+docker compose up -d
+
+RabbitMQ Login/Password:
+guest
+guest
---
+++ app.py
@@ -0,0 +1,43 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from json import dumps
+'''
+Написати додаток-вебсайт, який:
+
+    1) Рахувати кількість відвідувачів
+    2) Виводити декілька останніх відвідувачів
+
+http server
+
+redis
+mongo
+rabbitmq
+'''
+
+visitors_count = 0
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def __init__(self, request, client_address, server_class):
+        self.server_class = server_class
+        super().__init__(request, client_address, server_class)
+
+    def do_GET(self):
+        global visitors_count
+        visitors_count += 1
+        response = f"""
+            <h2>Welcome to our website!</h2>
+            <p>Number of visitors is <b>{visitors_count}</b>!</p>
+        """
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.send_header("Content-Length", str(len(dumps(response))))
+        self.end_headers()
+        self.wfile.write(str(response).encode('utf8'))
+
+def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    print("Starting web server...")
+    run(handler_class=RequestHandler)
